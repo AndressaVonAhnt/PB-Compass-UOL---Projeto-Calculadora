f9375c1079c17e06d09bdd737720602643f9e151
Andressa Von Ahnt <andressavahnt@gmail.com>
2025-09-12 13:54:05 -0300

    test: Adiciona testes para a multiplicacao (casos de zero, um e negativos)

--- test_calculadora.py
+++ test_calculadora.py
@@ -15,3 +15,28 @@ def test_subtracao_de_dois_numeros_resultado_negativo():
     calculadora = Calculadora()
     resultado = calculadora.subtrair(5,9)
     assert resultado == -4
+
+def test_multiplicacao_de_dois_numeros_positivos():
+    calculadora = Calculadora()
+    resultado = calculadora.multiplicar(2,9)
+    assert resultado == 18
+
+def test_multiplicacao_de_dois_numeros_negativos():
+    calculadora = Calculadora()
+    resultado = calculadora.multiplicar(-3,-2)
+    assert resultado == 6
+
+def test_multiplicacao_de_um_positivo_um_negativo():
+    calculadora = Calculadora()
+    resultado = calculadora.multiplicar(4,-2)
+    assert resultado == -8
+
+def test_multiplicacao_por_zero():
+    calculadora = Calculadora()
+    resultado = calculadora.multiplicar(6,0)
+    assert resultado == 0
+
+def test_multiplicacao_por_1():
+    calculadora = Calculadora()
+    resultado = calculadora.multiplicar(7,1)
+    assert resultado == 7
